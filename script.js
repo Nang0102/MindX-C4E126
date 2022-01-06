5d5a94c6fcaaed5a8f8a95324279080b4c8ada03
Nang0102 <ntmainbm6328@gmail.com>
2022-01-06 15:50:35 +0700

    btvn Java Script

---
+++ script.js
@@ -0,0 +1,19 @@
+let d = prompt("nhap duong kinh hinh tron")
+const pi = 3.14
+console.log(d*pi)
+
+let a = prompt("nhap ho va ten cua ban")
+console.log(a.length)
+console.log(a.toUpperCase())
+
+let b = prompt("nhap mat khau cua ban")
+if(console.log(b.length)>=8){    alert("mật khẩu phù hợp")} 
+else{    alert("mật khẩu không phù hợp")}
+
+
+let c = Number(prompt("nhap vao mot thang bat ki"))
+if(c=1;c=2; c=3){ alert(day la mua xuan)}
+else{
+    
+}
+
