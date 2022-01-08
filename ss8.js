2e2329d06fbb3b259f336993451e67ac5950ec2e
Nang0102 <ntmainbm6328@gmail.com>
2022-01-08 22:24:45 +0700

    Update and delete control files

--- ss8.js
+++ ss8.js
@@ -1,8 +1,8 @@
 console.log("nhap vao mot phep tinh");
 
-let a = prompt("phep tinh thuc hien boi =, -, *,/");
-let b = Number(promt("nhap so thu nhat"));
-let c = Number(promt("nhap so thu hai"));
+let a = prompt("phep tinh thuc hien boi +, -, *,/");
+let b = Number(prompt("nhap so thu nhat"));
+let c = Number(prompt("nhap so thu hai"));
 
 
 let result
@@ -21,13 +21,13 @@ if (a==="/") { result = a / b
 
 // bài 2 tìm số nguyên tố
 
-let a = Number(promt("nhap vao mot so bat ki"));
+// let a = Number(promt("nhap vao mot so bat ki"));
 
-if( a===2 ) { console.log("a la so nguyen to") = true}
-if (a <2 ) { console.log("a la so nguyen to"= false}
-for( let a>2, a++){ console.log("a la so nguyento")}
-   if(a %1===a && a%a===1) { console.log("a la so nguyen to") = true}
-   esle{
-       console.log("a la so nguyen to") = false
-   } { break;}
-   so nguyen to la: a
+// if( a===2 ) { console.log("a la so nguyen to") = true}
+// if (a <2 ) { console.log("a la so nguyen to"= false}
+// for( let a>2, a++){ console.log("a la so nguyento")}
+//    if(a %1===a && a%a===1) { console.log("a la so nguyen to") = true}
+//    esle{
+//        console.log("a la so nguyen to") = false
+//    } { break;}
+ 
